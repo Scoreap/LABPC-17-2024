c1482008f4b62d25047b168cff5631262da8c147
Scoreap <157499534+Scoreap@users.noreply.github.com>
2024-02-27 09:24:40 -0600

    Add files via upload

---
+++ Semana7/Program.cs
@@ -0,0 +1,25 @@
+﻿class Semana7
+{
+    static void Main(string[] args)
+    {
+        string sNombre = " ";
+        string sEdad = " ";
+        string sCarrera = " ";
+        string sCarne = " ";
+
+        Console.WriteLine("Mi segundo programa");
+        Console.WriteLine("Ingrese su nombre: ");
+        sNombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad: ");
+        sEdad = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carrera: ");
+        sCarrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su número de Carnet: ");
+        sCarne = Console.ReadLine();
+
+        Console.ReadKey();
+    } 
+}
