a35a48e3ef84f0195e7e54d49583001be50fb62a
Scoreap <157499534+Scoreap@users.noreply.github.com>
2024-03-05 15:40:46 -0600

    Add files via upload

---
+++ Semana-7-3/DespejedeF.cs
@@ -0,0 +1,29 @@
+﻿namespace Labsemana7
+{
+    class DespejedeF
+    {
+        static void Main(string [] args)
+        {
+            int vf = 0;
+            int a = 0;
+            int t = 0;
+            
+
+            Console.WriteLine("Ingrese la aceleración: ");
+                a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tiempo: ");
+                t = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la velocidad final: ");
+                vf = Int32.Parse(Console.ReadLine());
+
+            int vi = vf - (a*t);
+
+            Console.Write("La velocidad inicial = " + vi);
+
+            Console.ReadKey();
+
+        }
+    }
+}
